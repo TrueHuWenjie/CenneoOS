182cf86ff98879cb26e58a77f8bcd7eab85f994e
HuWenjie <me@cenneo.com>
2020-02-08 09:11:36 -0800

    Optimaize file structure

--- loader/boot/arch/x86/interrupt/entry.asm
+++ loader/boot/arch/x86/interrupt/entry.asm
@@ -3,11 +3,80 @@
 ; Interruption's entry
 
 ; Globally visible
-
+extern	keyboard_handle
+extern	i8259A_auto_return
+extern	int_auto_return
+global	int_auto
+global	int_0x20
+global	int_0x21
+global	int_0x22
+global	int_0x23
+global	int_0x24
+global	int_0x25
+global	int_0x26
+global	int_0x27
+global	int_0x28
+global	int_0x29
+global	int_0x2A
+global	int_0x2B
+global	int_0x2C
+global	int_0x2D
+global	int_0x2E
+global	int_0x2F
 
 ; Functions
 [bits 32]
 [section .text]
 
-
-
+; The entry of Interruption
+int_auto:
+	call	int_auto_return
+	iret
+int_0x20:
+	call	i8259A_auto_return
+	iret
+int_0x21:
+	call	keyboard_handle
+	iret
+int_0x22:
+	call	i8259A_auto_return
+	iret
+int_0x23:
+	call	i8259A_auto_return
+	iret
+int_0x24:
+	call	i8259A_auto_return
+	iret
+int_0x25:
+	call	i8259A_auto_return
+	iret
+int_0x26:
+	call	i8259A_auto_return
+	iret
+int_0x27:
+	call	i8259A_auto_return
+	iret
+int_0x28:
+	call	i8259A_auto_return
+	iret
+int_0x29:
+	call	i8259A_auto_return
+	iret
+int_0x2A:
+	call	i8259A_auto_return
+	iret
+int_0x2B:
+	call	i8259A_auto_return
+	iret
+int_0x2C:
+	call	i8259A_auto_return
+	iret
+int_0x2D:
+	call	i8259A_auto_return
+	iret
+int_0x2E:
+	call	i8259A_auto_return
+	iret
+int_0x2F:
+	call	i8259A_auto_return
+	iret
--- loader/boot/boota.asm
+++ loader/boot/boota.asm
@@ -2,49 +2,15 @@
 ; /boot/boota.asm
 ; ASM file of the Explorer Boot
 
-
-; 起始及初始化函数
-global	init_IDT
-
 extern IDTR.base
-
-; 中断入口函数
-extern	keyboard_handle
-extern	i8259A_auto_return
-extern	int_auto_return
-
-global	int_auto
-global	int_0x20
-global	int_0x21
-global	int_0x22
-global	int_0x23
-global	int_0x24
-global	int_0x25
-global	int_0x26
-global	int_0x27
-global	int_0x28
-global	int_0x29
-global	int_0x2A
-global	int_0x2B
-global	int_0x2C
-global	int_0x2D
-global	int_0x2E
-global	int_0x2F
-
-; 描述符寄存器操作函数
-global	write_IDTR
 
 ; IDT操作函数
 global	create_ID
-
-
 
 [bits 32]
 ; Code section
 
 [section .text]
-
-	
 ; 创建一个中断描述符函数*/
 create_ID:
 ;void create_ID(u32 number, u32 selector, u32 offset, u32 attribute)
@@ -62,57 +28,3 @@ create_ID:
 	mov		edx,[esp+16];attribute参数
 	add		[eax+4],edx	;加上属性
 	ret
-	
-; 中断入口函数
-int_auto:
-	call	int_auto_return
-	iret
-int_0x20:
-	call	i8259A_auto_return
-	iret
-int_0x21:
-	call	keyboard_handle
-	iret
-int_0x22:
-	call	i8259A_auto_return
-	iret
-int_0x23:
-	call	i8259A_auto_return
-	iret
-int_0x24:
-	call	i8259A_auto_return
-	iret
-int_0x25:
-	call	i8259A_auto_return
-	iret
-int_0x26:
-	call	i8259A_auto_return
-	iret
-int_0x27:
-	call	i8259A_auto_return
-	iret
-int_0x28:
-	call	i8259A_auto_return
-	iret
-int_0x29:
-	call	i8259A_auto_return
-	iret
-int_0x2A:
-	call	i8259A_auto_return
-	iret
-int_0x2B:
-	call	i8259A_auto_return
-	iret
-int_0x2C:
-	call	i8259A_auto_return
-	iret
-int_0x2D:
-	call	i8259A_auto_return
-	iret
-int_0x2E:
-	call	i8259A_auto_return
-	iret
-int_0x2F:
-	call	i8259A_auto_return
-	iret
-
