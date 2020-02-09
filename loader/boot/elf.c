6793d1099925882973ba374cdf18fb09085aecc1
HuWenjie <me@cenneo.com>
2020-02-09 10:21:23 -0800

    Optimize for tab width 4

--- loader/boot/elf.c
+++ loader/boot/elf.c
@@ -35,40 +35,40 @@ typedef struct {
 #define ET_HIPROC	0xffff		// Processor-specific
 
 // e_machine
-#define EM_NONE		ET_NONE		// No machine
-#define EM_M32		1		// AT&T WE 32100
+#define EM_NONE			ET_NONE	// No machine
+#define EM_M32			1		// AT&T WE 32100
 #define EM_SPARC		2		// SPARC
-#define EM_386		3		// Intel Architecture
-#define EM_68K		4		// Motorola 68000
-#define EM_88K		5		// Motorola 88000
-#define EM_860		7		// Intel 80860
-#define EM_MIPS		8		// MIPS RS3000 Big-Endian
+#define EM_386			3		// Intel Architecture
+#define EM_68K			4		// Motorola 68000
+#define EM_88K			5		// Motorola 88000
+#define EM_860			7		// Intel 80860
+#define EM_MIPS			8		// MIPS RS3000 Big-Endian
 #define EM_MIPS_RS4_BE	10		// MiPS RS4000 Big-Endian
 
 // e_version
-#define EV_NONE		0		// Invalid version
-#define EV_CURRENT	1		// Current version
+#define EV_NONE			0		// Invalid version
+#define EV_CURRENT		1		// Current version
 
 // e_ident
-#define EI_MAG0		0		// File identification
-#define EI_MAG1		1		// File identification
-#define EI_MAG2		2		// File identification
-#define EI_MAG3		3		// File identification
-#define EI_CLASS	4		// File class
-#define EI_DATA		5		// Data encoding
-#define EI_VERSION	6		// File version
-#define EI_PAD		7		// Start of padding bytes
-#define EI_NIDENT	16		// Size of e_ident[]
+#define EI_MAG0			0		// File identification
+#define EI_MAG1			1		// File identification
+#define EI_MAG2			2		// File identification
+#define EI_MAG3			3		// File identification
+#define EI_CLASS		4		// File class
+#define EI_DATA			5		// Data encoding
+#define EI_VERSION		6		// File version
+#define EI_PAD			7		// Start of padding bytes
+#define EI_NIDENT		16		// Size of e_ident[]
 
 // e_ident[EI_CLASS]
 #define ELFCLASSNONE	0		// Invalid class
-#define ELFCLASS32	1		// 32-bit objects
-#define ELFCLASS64	2		// 64-bit objects
+#define ELFCLASS32		1		// 32-bit objects
+#define ELFCLASS64		2		// 64-bit objects
 
 // e_ident[EI_DATA]
-#define ELFDATANONE	0		// Invalid data encoding
-#define ELFDATA2LSB	1
-#define ELFDATA2MSB	2
+#define ELFDATANONE		0		// Invalid data encoding
+#define ELFDATA2LSB		1
+#define ELFDATA2MSB		2
 
 // Section Header
 typedef struct {
@@ -87,4 +87,3 @@ typedef struct {
 #define ELF32_ST_BIND(i) ((i)>>4)
 #define ELF32_ST_TYPE(i) ((i)&0xf)
 #define ELF32_ST_INFO(b,t) (((b)<<4)+((t)&0xf))
-
